dcb4054b733b28179a641a91b4393d2e96b34438
ignjas <ignas.jasonas@gmail.com>
2019-12-11 00:29:45 +0200

    small updates

--- src/components/layouts/Layout/component.tsx
+++ src/components/layouts/Layout/component.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Header } from '..'
+import { Flex } from '../../atoms'
 
 const layoutStyle = {
   minHeight: '100vh',
   width: '100%',
+  padding: 20,
 }
 
 export const Layout = ({ children }) => (
   <div style={layoutStyle}>
     <Header />
-    {children}
+    <Flex>{children}</Flex>
   </div>
 )
 
