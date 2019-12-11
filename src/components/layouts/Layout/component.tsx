e704bf2d8cd1ca3ffb4579ede681d6e89fa55ee9
ignjas <ignas.jasonas@gmail.com>
2019-12-11 17:58:19 +0200

    update layout component

--- src/components/layouts/Layout/component.tsx
+++ src/components/layouts/Layout/component.tsx
@@ -5,11 +5,22 @@ import { Flex } from '../../atoms'
 export const Layout = ({ children }) => (
   <div>
     <Header />
-    <Flex>{children}</Flex>
+    <Flex justifyContent="center" grow="1" className="content-container">
+      {children}
+    </Flex>
     <style jsx>{`
-      min-height: 100vh;
-      width: 100%;
-      box-sizing: border-box;
+      div {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        width: 100%;
+        box-sizing: border-box;
+      }
+    `}</style>
+    <style jsx global>{`
+      .content-container {
+        padding: 0 20px;
+      }
     `}</style>
   </div>
 )
