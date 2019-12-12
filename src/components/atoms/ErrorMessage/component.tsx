a996c46cac81fe1c958707c503703a8a2adb9dd7
ignjas <ignas.jasonas@gmail.com>
2019-12-12 05:27:29 +0200

    update error message styles

--- src/components/atoms/ErrorMessage/component.tsx
+++ src/components/atoms/ErrorMessage/component.tsx
@@ -8,11 +8,13 @@ export const ErrorMessage = ({ children }) => (
     <style jsx>
       {`
       span {
-        width: '100%',
-        minHeight: '19px',
-        textAlign: 'left',
-        whiteSpace: 'pre-wrap',
-        boxSizing: 'content-box',
+        width: 100%;
+        min-height: 19px;
+        text-align: left;
+        white-space: pre-wrap;
+        box-sizing: content-box;
+        color: red;
+        font-size: 14px;
       }`}
     </style>
   </React.Fragment>
