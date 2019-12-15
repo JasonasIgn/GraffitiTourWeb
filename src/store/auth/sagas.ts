f8ba75bcad4ae119e6936622053fdf6057b64f7a
ignjas <ignas.jasonas@gmail.com>
2019-12-15 20:08:46 +0200

    request profile when logging in

--- src/store/auth/sagas.ts
+++ src/store/auth/sagas.ts
@@ -1,6 +1,7 @@
 import { put, call, takeEvery, take } from 'redux-saga/effects'
 import Router from 'next/router'
 import * as actions from './actions'
+import * as userActions from '../users/actions'
 import { api } from '../../utils/api'
 import config from '../../config'
 import { AuthActionTypes } from './types'
@@ -12,6 +13,7 @@ export function* loginRequest(data) {
     const payload = yield call(api, config.apiMethods.POST, 'login', data)
     yield put(actions.loginRequestSuccess(payload))
     setAuthTokens(payload)
+    yield put(userActions.profileRequest())
     Router.push(pages.homepage.path)
   } catch (e) {
     const error = yield e
